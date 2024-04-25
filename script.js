ab0979f79170a5b78405546bf8a7f7bf1a6d89d9
Mikaela Bell <19698567+mikaelabelle@users.noreply.github.com>
2024-04-25 17:30:33 +1000

    more js

--- script.js
+++ script.js
@@ -8,7 +8,31 @@ const gameBoard = (function () {
         }
     }
 
-    return { board }
+    const printBoard = () => {
+        board.forEach(row => console.log(...row))
+        console.log("===========")
+    }
+
+    return { printBoard, board }
 })()
 
-gameBoard.board.forEach(row => console.log(...row))
+function gameController() {
+    const playerTurn = (row, col) => {
+        gameBoard.board[row - 1][col - 1] = "[X]"
+        gameBoard.printBoard()
+    }
+
+    const createPlayer = (playerNum) => {
+        const name = `Player ${playerNum}`
+        return { name, playerNum }
+    }
+
+    const player1 = createPlayer(1)
+    const player2 = createPlayer(2)
+
+    return { playerTurn }
+}
+
+gameController()
+gameBoard.printBoard()
+gameController.playerTurn()
